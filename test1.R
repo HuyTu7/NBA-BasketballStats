f4183234cdce3ab38fba441ac648e177028cc6a6
Aravind Anantha <aravind.mufc@gmail.com>
2016-11-06 16:02:23 -0500

    multivariate linear regression model which sucks :D

--- test1.R
+++ test1.R
@@ -5,20 +5,44 @@ library(e1071)
 data = read.csv("team_season.csv", header = T)
 #data
 
-wins = data$won/(data$won+data$lost)
+wins = data$won*100/(data$won+data$lost)
 #wins
 
 components = preProcess(data[,3:33], method=c("center", "scale", "pca"))
 
-pcaData = predict(components, data[,3:33])
+pcaData = cbind(predict(components, data[,3:33]), wins)
 
+pcaData[1,]
 
-model = lm(wins~pcaData[,1]+pcaData[,2]+pcaData[,3]+pcaData[,4]+pcaData[,5]
-           +pcaData[,6]+pcaData[,7]+pcaData[,8]+pcaData[,9]+pcaData[,10])
+## 75% of the sample size
+smp_size <- floor(0.75 * nrow(pcaData))
 
-plot(pcaData[,1],wins)
-lines(pcaData[,1],predict(model),lty=2,col="red")
+## set the seed to make your partition reproductible
+set.seed(123)
+train_ind <- sample(seq_len(nrow(pcaData)), size = smp_size)
 
-e = resid(model)
+train <- pcaData[train_ind, ]
+test <- pcaData[-train_ind, ]
 
-plot(pcaData[,1], e)
+model = lm(wins~PC1+PC2+PC3+PC4+PC5+PC6+PC7+PC8+PC9+PC10, data = train)
+
+model
+
+plot(train[,1], train[,11])
+plot(train[,2], train[,11])
+plot(train[,3], train[,11])
+plot(train[,4], train[,11])
+
+outcomes = predict.lm(model, test[,1:10])
+
+trainOutput = predict.lm(model)
+trainError = sum((trainOutput - train[,11])^2)/513
+trainError
+
+head(outcomes)
+
+head(test[,11])
+
+testError = sum((outcomes - test[,11])^2)/171
+
+testError
