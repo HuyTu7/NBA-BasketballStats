773b9e9522d840679e30e9ffb98cc5e3bb0a3e8a
Aravind Anantha <aravind.mufc@gmail.com>
2016-11-06 13:47:40 -0500

    updated pca code and modified the data file a bit

--- test1.R
+++ test1.R
@@ -1,7 +1,24 @@
+library(caret)
+library(ggplot2)
+library(lattice)
+library(e1071)
 data = read.csv("team_season.csv", header = T)
 #data
-data[1:2, 2:36]
 
-pca = prcomp(data[,4:36], scale. = T, center = T)
+wins = data$won/(data$won+data$lost)
+#wins
 
-summary(pca)
+components = preProcess(data[,3:33], method=c("center", "scale", "pca"))
+
+pcaData = predict(components, data[,3:33])
+
+
+model = lm(wins~pcaData[,1]+pcaData[,2]+pcaData[,3]+pcaData[,4]+pcaData[,5]
+           +pcaData[,6]+pcaData[,7]+pcaData[,8]+pcaData[,9]+pcaData[,10])
+
+plot(pcaData[,1],wins)
+lines(pcaData[,1],predict(model),lty=2,col="red")
+
+e = resid(model)
+
+plot(pcaData[,1], e)
